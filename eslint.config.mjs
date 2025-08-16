bf7a161b9059c3fdfd9eba7d74806c046c12c525
Mohammad Yasin <mohammadyasinbappy@outlook.com>
2025-08-16 17:18:13 +0600

    trying with eslintConfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "app/generated/prisma/",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
