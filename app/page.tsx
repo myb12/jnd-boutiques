2b84ff3e7e1304304fbb269510fb5529ea5995f4
Mohammad Yasin <mohammadyasinbappy@outlook.com>
2025-08-18 13:27:17 +0600

    Caregories build

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,14 @@
 "use client"
 
+import Categories from '@/components/Home/Categories';
 import Hero from '@/components/Home/Hero';
-import { useState, FormEvent } from 'react';
-// Main component for the entire app. Export this as default.
+
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-
-  // Simple state to simulate user login.
-  const handleLogin = (e: FormEvent) => {
-    e.preventDefault();
-    // In a real app, you would validate credentials here.
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
 
   return (
     <main>
       <Hero/>
+      <Categories/>
     </main>
   );
 }
